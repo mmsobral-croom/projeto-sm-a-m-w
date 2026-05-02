a47099703a43cbe5c3aec1185afd5022c9b6d8c7
Willian Ferreira dos Santos <willsantfer@gmail.com>
2026-05-02 20:36:12 -0300

    Cria a classe Cesta em src/main/java/app

--- src/main/java/app/Cesta.java
+++ src/main/java/app/Cesta.java
@@ -1,4 +1,22 @@
 package app;
 
+import esd.ListaSequencial;
+
 public class Cesta {
+
+    private ListaSequencial<ItemCesta> itens;
+
+    public Cesta() {
+        this.itens = new ListaSequencial<>();
+    }
+
+    public ListaSequencial<ItemCesta> getItens() {
+        return this.itens;
+    }
+
+    public void adicionaItem(ItemCesta item) {
+        if (item != null) {
+            itens.adiciona(item);
+        }
+    }
 }
