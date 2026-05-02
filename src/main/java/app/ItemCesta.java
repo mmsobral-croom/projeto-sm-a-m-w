e74638b2b83c1a859d0bb125c334d3cfe4b66a22
Willian Ferreira dos Santos <willsantfer@gmail.com>
2026-05-02 20:57:52 -0300

    Adiciona o atributo marcas a classe ItemCesta. É uma lista para adicionar mais de uma marca na busca de um produto

--- src/main/java/app/ItemCesta.java
+++ src/main/java/app/ItemCesta.java
@@ -1,18 +1,24 @@
 package app;
 
+import esd.ListaSequencial;
+
 public class ItemCesta {    // o objeto instanciado desta classe vai representar um item (produto) que será colocado na cesta
 
     private String descricao;
     private String tamanho;
 
+    private ListaSequencial<String> marcas;     // lista para selecionar mais de uma marca na busca de um produto
+
     public ItemCesta(String descricao) {
-        this.descricao = descricao;
+        this.descricao = descricao.toLowerCase();   // toLowerCase() para aceitar letras maiúsculas ou minúsculas
         this.tamanho = null;
+        this.marcas = new ListaSequencial<>();
     }
 
     public ItemCesta(String descricao, String tamanho) {
-        this.descricao = descricao;
+        this.descricao = descricao.toLowerCase();
         this.tamanho = tamanho;
+        this.marcas = new ListaSequencial<>();
     }
 
     public String getDescricao() {
@@ -21,5 +27,15 @@ public class ItemCesta {    // o objeto instanciado desta classe vai representar
 
     public String getTamanho() {
         return this.tamanho;
+    }
+
+    public ListaSequencial<String> getMarcas() {
+        return this.marcas;
+    }
+
+    public void adicionarMarca(String marca) {
+        if (marca != null) {
+            marcas.adiciona(marca.toLowerCase());
+        }
     }
 }
