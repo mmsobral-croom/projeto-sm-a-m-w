5651303747e651d6c29f31c9dcb59154833f4489
Willian Ferreira dos Santos <willsantfer@gmail.com>
2026-06-23 13:40:58 -0300

    Atualiza o método main da classe Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
-import app.Cesta;
-import app.ItemCesta;
-import app.TotalMercado;
+import app.*;
 import esd.ListaSequencial;
 import sm.*;
 
 public class Main {
 
     static void main() {
+
+        // teste de tempo de execução - inicio
+        long inicio = System.currentTimeMillis();
+
+        CacheSupermercado cache = new CacheSupermercado(CachePersistencia.carregar("cache.txt"));
+
+        ItemCesta.setCache(cache);
+
         Cesta cesta = Cesta.montaCesta();
 
         if (cesta.comprimento() == 0) {
@@ -18,12 +24,22 @@ public class Main {
         ListaSequencial<TotalMercado> ranking = calculaRanking(cesta, supermercados);
 
         if (ranking.esta_vazia()) {
+
             System.out.println("Nenhum supermercado tem todos os itens da cesta.");
+
+            CachePersistencia.salvar("cache.txt", cache.getCacheBusca());
+
             return;
         }
 
         mostraRanking(ranking);
         mostraDetalheCestaMaisBarata(cesta, ranking.obtem(0));
+
+        CachePersistencia.salvar("cache.txt", cache.getCacheBusca());
+
+        // teste de tempo de execução - fim
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo: " + ((fim - inicio) / 1000) + " s");
     }
 
     // calcula o preço total da cesta em cada supermercado e devolve a lista ordenada
